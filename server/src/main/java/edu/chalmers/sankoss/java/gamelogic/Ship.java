1a2004b98643f06f27088098b0ed3c73595a867a
Daniel Eineving <daniel@eineving.se>
2014-03-27 19:49:40 +0100

    Made changes to gamelogic classes on the server

--- server/src/main/java/edu/chalmers/sankoss/java/gamelogic/Ship.java
+++ server/src/main/java/edu/chalmers/sankoss/java/gamelogic/Ship.java
@@ -6,14 +6,14 @@ import java.util.LinkedList;
  * Class representing a ship
  * @author Daniel Eineving
  * @modified Niklas Tegnander
+ * @date 2014-03-27
  */
 
 public class Ship {
 	private int size;
 	private Coordinate start, end;
 	private int hits=0;
-	//TODO Shall the ship keep track WHERE it is hit?
-	
+
 	/**
 	 * Creates a Ship
 	 * @param size - Grid length of the Ship
@@ -22,53 +22,81 @@ public class Ship {
 	 */
 	public Ship(int size, Coordinate start, Coordinate end){
 		this.size=size;
-		
-		// TODO No real need to swap start with end? Should be able to rotate ship 180 degrees?
-		//Makes sure start is either above or to the left of the end coordinate
-		if(start.getX()+start.getY() > end.getX() + end.getY()){
-			this.start=end;
-			this.end=start;
-		}
-		else{
-			this.start=start;
-			this.end=end;
-		}
-
+		this.start=start;
+		this.end=end;
 	}
-	
+
 	/**
 	 * Gets all the boats coordinates as a LinkedList
 	 * @return The coordinates of the boat.
 	 */
 	public LinkedList<Coordinate> getCoordinates(){
 		LinkedList<Coordinate> coordinates = new LinkedList<Coordinate>();
-		
+
 		coordinates.add(start);
+
 		if(start.getX()==end.getX()){
-			for(int i=1;i<(size-1);i++){
-				coordinates.add(new Coordinate(start.getX(), start.getY()+i));
+			if(start.getY()<end.getX()){
+				for(int i=1;i<(size-1);i++){
+					coordinates.add(new Coordinate(start.getX(), start.getY()+i));
+				}
+			}
+			else{
+				for(int i=1;i<(size-1);i++){
+					coordinates.add(new Coordinate(start.getX(), start.getY()-i));
+				}
 			}
 		}
 		else{
-			for(int i=1;i<(size-1);i++){
-				coordinates.add(new Coordinate(start.getX()+i, start.getY()));
+			if(start.getY()<end.getX()){
+				for(int i=1;i<(size-1);i++){
+					coordinates.add(new Coordinate(start.getX()+i, start.getY()));
+				}
+			}
+			else{
+				for(int i=1;i<(size-1);i++){
+					coordinates.add(new Coordinate(start.getX()-i, start.getY()));
+				}
 			}
 		}
 		coordinates.add(end);
-		
+
 		return coordinates;
 	}
-	
+
+	/**
+	 * Checks if a target is a part of the ship
+	 * @param target
+	 * @return True if the target coordinate is a part of the ship
+	 */
 	public boolean isShip(Coordinate target){
-		//TODO split into different parts?
-		
-		//TODO How do we count our coordinates?
+		LinkedList<Coordinate> temp = getCoordinates();
+
+		for(int i=0;i<size;i++){
+			if(target.equals(temp.get(i))){
+				return true;
+			}
+		}
+		return false;
+
+		/*
 		return (start.getX()==end.getX() && target.getX()==start.getX() && start.getY() <= target.getY() && target.getY() <=end.getY()) 
 				|| (start.getY()==end.getY() && target.getY()==start.getY() && start.getX() <= target.getX() && target.getX() <=end.getX());
+		 */
 	}
-	
+
+	/**
+	 * Adds a hit to the ship
+	 */
 	public void shipHit(){
-		//TODO Shall we save a coordinate?
 		hits++;
+	}
+
+	/**
+	 * Checks if the ship is destroyed
+	 * @return True if the boat is destroyed
+	 */
+	public boolean isDestroyed(){
+		return hits >= size;
 	}
 }
