03969ac5e7d1aaf2d779621c57aa2cb520bb83d0
Daniel Eineving <daniel@eineving.se>
2014-04-01 18:18:52 +0200

    Removed Ship Icon

--- desktop/src/main/java/edu/chalmers/sankoss/java/Models/Lobby.java
+++ desktop/src/main/java/edu/chalmers/sankoss/java/Models/Lobby.java
@@ -1,6 +1,6 @@
 package edu.chalmers.sankoss.java.Models;
 
-import edu.chalmers.sankoss.core.protocol.Room;
+//import edu.chalmers.sankoss.core.protocol.Room;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,10 +13,10 @@ import java.util.Map;
  * @date 3/24/14
  */
 public class Lobby extends ScreenModel {
-    private Map<Long, Room> rooms = new HashMap<Long, Room>();
+    /*private Map<Long, Room> rooms = new HashMap<Long, Room>();
 
     public Map<Long, Room> getRoomMap() {
         return this.rooms;
-    }
+    }*/
 
 }
