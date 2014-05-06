c324547043a8a8e407ca5937ec66ccb322dcb2c6
Thune <fredrik.thune.94@gmail.com>
2014-05-06 21:16:21 +0200

    Removed player from core and made a SankossClientPlayer

---
+++ desktop/src/main/java/edu/chalmers/sankoss/java/client/SankossClientPlayer.java
@@ -0,0 +1,30 @@
+package edu.chalmers.sankoss.java.client;
+
+import edu.chalmers.sankoss.core.BasePlayer;
+import edu.chalmers.sankoss.core.Fleet;
+
+/**
+ * @author Fredrik Thune
+ */
+public class SankossClientPlayer extends BasePlayer{
+    private Fleet fleet = new Fleet();
+
+    public SankossClientPlayer() {
+    }
+
+    public SankossClientPlayer(Long playerID) {
+        super(playerID);
+    }
+
+    public Fleet getFleet() {
+        return fleet;
+    }
+
+    public void setFleet(Fleet fleet) {
+        this.fleet = fleet;
+    }
+
+    public BasePlayer getBasePlayer() {
+        return this;
+    }
+}
