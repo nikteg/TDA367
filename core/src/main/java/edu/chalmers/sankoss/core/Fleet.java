49694cc792c5c471206ccdf5d9ddb90c4337a561
Thune <fredrik.thune.94@gmail.com>
2014-05-06 21:14:28 +0200

    Added fleet class with validation

---
+++ core/src/main/java/edu/chalmers/sankoss/core/Fleet.java
@@ -0,0 +1,52 @@
+package edu.chalmers.sankoss.core;
+
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Fredrik Thune
+ *
+ */
+public class Fleet {
+    private ArrayList<Ship> ships = new ArrayList<Ship>();
+    private int size = 5;
+    public Fleet() {
+    }
+
+    public Fleet(int size) {
+        this.size = size;
+    }
+
+    public boolean add(Ship ship) {
+        return (((ships.size() + 1) > size) && validate(ship) && ships.add(ship));
+    }
+
+    public boolean remove(Ship ship) {
+        return ships.remove(ship);
+    }
+
+    public List<Ship> getShips() {
+        return new ArrayList<Ship>(ships);
+    }
+
+    public Ship getShip(int index) {
+        return ships.get(index);
+    }
+
+    private boolean validate(Ship ship) {
+
+        Line2D shipLine = new Line2D.Double(ship.getFront().getX(), ship.getFront().getY(),
+                                            ship.getRear().getX(), ship.getRear().getY());
+
+        for (Ship otherShip : ships) {
+            Line2D otherShipLine = new Line2D.Double(otherShip.getFront().getX(), otherShip.getFront().getY(),
+                                                     otherShip.getRear().getX(), otherShip.getRear().getY());
+            if(otherShipLine.intersectsLine(shipLine))
+                return false;
+
+        }
+        return true;
+    }
+}
