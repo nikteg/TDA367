0fba484c56655a887a78445c773d187387022849
Thune <fredrik.thune.94@gmail.com>
2014-05-18 00:52:55 +0200

    Added leave game and room protocols

---
+++ core/src/main/java/edu/chalmers/sankoss/core/protocol/LeftGame.java
@@ -0,0 +1,26 @@
+package edu.chalmers.sankoss.core.protocol;
+
+import edu.chalmers.sankoss.core.CorePlayer;
+
+/**
+ * Will be sent to the host of the left game
+ * @author Fredrik Thune
+ */
+public class LeftGame {
+    private CorePlayer player;
+
+    public LeftGame() {
+    }
+
+    public LeftGame(CorePlayer player) {
+        this.player = player;
+    }
+
+    public CorePlayer getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(CorePlayer player) {
+        this.player = player;
+    }
+}
