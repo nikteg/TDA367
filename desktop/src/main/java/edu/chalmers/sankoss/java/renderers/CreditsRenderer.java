c7193a7df0be00e7b9a3384ca73a134f8fdb66c5
Niklas Tegnander <bipshark@gmail.com>
2014-05-19 13:35:39 +0200

    Finished credits screen

---
+++ desktop/src/main/java/edu/chalmers/sankoss/java/renderers/CreditsRenderer.java
@@ -0,0 +1,71 @@
+package edu.chalmers.sankoss.java.renderers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import edu.chalmers.sankoss.java.SankossGame;
+import edu.chalmers.sankoss.java.Screens;
+import edu.chalmers.sankoss.java.models.CreditsModel;
+
+import java.util.Observable;
+
+/**
+ * AbstractRenderer for the Credit text
+ * 
+ * @author Daniel Eineving
+ * @date 2014-05-13
+ */
+public class CreditsRenderer extends AbstractRenderer {
+
+    private final float scrollSpeed = 0.01f;
+
+    TextButton btnBack = new TextButton("Back", SankossGame.getInstance().getSkin());
+    Label lblCredits = new Label("", SankossGame.getInstance().getSkin());
+
+    public CreditsRenderer(Observable observable) {
+        super(observable);
+
+        lblCredits.setText(((CreditsModel) observable).getCreditsText());
+        lblCredits.setAlignment(Align.center);
+        lblCredits.setX(getStage().getViewport().getViewportWidth() / 2 - lblCredits.getWidth() / 2);
+        //lblCredits.setY(-500);
+
+        getTable().pad(8f);
+        getTable().add(btnBack).expand().bottom().left();
+        //getTable().debug();
+
+        getStage().addActor(lblCredits);
+        getStage().addActor(getTable());
+
+        btnBack.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Screens.MAIN_MENU.show();
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0.09f, 0.58f, 0.2f, 1);
+
+        lblCredits.setY(lblCredits.getY() + scrollSpeed / delta);
+
+        getStage().act(delta);
+        getStage().draw();
+        Table.drawDebug(getStage());
+    }
+
+    @Override
+    public void update(Observable object, Object arg) {
+        if (arg.equals("text_position")) {
+            lblCredits.setY(((CreditsModel)object).getCreditsTextPosition());
+        }
+    }
+}
