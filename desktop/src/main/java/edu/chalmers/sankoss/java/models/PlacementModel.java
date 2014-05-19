587ece3f8b3995aad27e6474c3d055d982cc10d4
Daniel Eineving <daniel@eineving.se>
2014-05-19 16:41:38 +0200

    Added more methods in the placement

--- desktop/src/main/java/edu/chalmers/sankoss/java/models/PlacementModel.java
+++ desktop/src/main/java/edu/chalmers/sankoss/java/models/PlacementModel.java
@@ -2,6 +2,7 @@ package edu.chalmers.sankoss.java.models;
 
 import java.awt.List;
 
+import edu.chalmers.sankoss.core.CorePlayer;
 import edu.chalmers.sankoss.core.Fleet;
 import edu.chalmers.sankoss.core.Ship;
 import edu.chalmers.sankoss.java.SankossGame;
@@ -12,20 +13,31 @@ import edu.chalmers.sankoss.java.SankossGame;
 public class PlacementModel extends AbstractModel {
 	private boolean opponentReady;
 	private Fleet fleet = new Fleet();
-	
-	public void setOpponentReady(boolean ready){
+	private CorePlayer.Nationality nationality = CorePlayer.Nationality.USA;
+
+	public void setOpponentReady(boolean ready) {
 		this.opponentReady = ready;
 	}
-	
-	public boolean isOpponentReady(){
+
+	public boolean isOpponentReady() {
 		return opponentReady;
 	}
-	
-	public void addShip(Ship ship){
+
+	public void addShip(Ship ship) {
 		fleet.add(ship);
 	}
-	
-	public List getShips(){
+
+	public List getShips() {
 		return (List) fleet.getShips();
+	}
+
+	public String getFlagPath() {
+		return nationality.getPath();
+	}
+
+	public void setNationality(CorePlayer.Nationality nationality) {
+		this.nationality = nationality;
+		setChanged();
+		notifyObservers("NationalityChanged");
 	}
 }
--- desktop/src/main/java/edu/chalmers/sankoss/java/renderers/PlacementRenderer.java
+++ desktop/src/main/java/edu/chalmers/sankoss/java/renderers/PlacementRenderer.java
@@ -2,13 +2,19 @@ package edu.chalmers.sankoss.java.renderers;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 import edu.chalmers.sankoss.java.SankossGame;
+import edu.chalmers.sankoss.java.models.PlacementModel;
 
 import java.util.Observable;
 
@@ -22,18 +28,50 @@ import java.util.Observable;
 public class PlacementRenderer extends AbstractRenderer {
 
 	TextButton btnReady = new TextButton("Ready", SankossGame.getInstance().getSkin());
+	TextButton btnNextFlag = new TextButton(">", SankossGame.getInstance().getSkin());
+	TextButton btnPreviousFlag = new TextButton("<", SankossGame.getInstance().getSkin());
+	Image grid = new Image(new Texture(Gdx.files.internal("textures/grid.png")));
+	Image flag = new Image();
 	
     public PlacementRenderer(Observable observable) {
         super(observable);
+        PlacementModel model = (PlacementModel) observable;
         
         btnReady.pad(8f);
+        btnNextFlag.pad(8f);
+        btnPreviousFlag.pad(8f);
         
-        //getTable().add(btnReady).fillX().pad(8f);
+        getTable().debug();
+        
+        getTable().add(grid);
         getTable().row();
+        
+        flag.setDrawable(new TextureRegionDrawable(new TextureRegion(new Texture(model.getFlagPath()))));
+        
+        getTable().add(flag);
         getTable().add(btnReady).fillX().pad(8f);
+        
+        getTable().add(btnNextFlag).pad(8f).bottom().left().expand();
+        getTable().add(btnPreviousFlag.pad(8f));
         getStage().addActor(getTable());
         
+        
+        
         btnReady.addListener(new ChangeListener(){
+			@Override
+			public void changed(ChangeEvent arg0, Actor arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        btnPreviousFlag.addListener(new ChangeListener(){
+			@Override
+			public void changed(ChangeEvent arg0, Actor arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        btnNextFlag.addListener(new ChangeListener(){
 			@Override
 			public void changed(ChangeEvent arg0, Actor arg1) {
 				// TODO Auto-generated method stub
@@ -54,6 +92,9 @@ public class PlacementRenderer extends AbstractRenderer {
 
     @Override
     public void update(Observable object, Object arg) {
-
+    	if(arg.equals("NationalityChanged")){
+    		flag.setDrawable(new TextureRegionDrawable(new TextureRegion(
+    				new Texture(((PlacementModel)object).getFlagPath()))));
+    	}
     }
 }
